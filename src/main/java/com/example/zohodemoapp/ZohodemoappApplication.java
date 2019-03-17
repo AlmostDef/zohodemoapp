4a77d0505c424727c29dca7dce0de5baba49c63b
AlmostDef <defperisher@gmail.com>
2019-03-17 18:27:56 +0200

    Initial commit

---
+++ src/main/java/com/example/zohodemoapp/ZohodemoappApplication.java
@@ -0,0 +1,13 @@
+package com.example.zohodemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZohodemoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZohodemoappApplication.class, args);
+    }
+
+}
